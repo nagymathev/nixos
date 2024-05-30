9dc27862282c56ec40b4e1cd54e1a893b87cb340
nagymathev <nagymathev@gmail.com>
2024-05-30 22:52:21 +0200

    no one needs gnome

--- desktop.nix
+++ desktop.nix
@@ -4,10 +4,6 @@
 # Enable the X11 windowing system.
 services.xserver.enable = true;
 
-# Enable the GNOME Desktop Environment.
-#services.xserver.displayManager.gdm.enable = true;
-#services.xserver.desktopManager.gnome.enable = true;
-
 # Enable the KDE Plasma Desktop Environment.
 services.desktopManager.plasma6.enable = true;
 services.displayManager.sddm.enable = true;
