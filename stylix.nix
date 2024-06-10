5318366368889f2e69d3eb5a389809966addac1f
nagymathev <nagymathev@gmail.com>
2024-06-10 14:40:13 +0200

    stylix to use gruvbox dark hard and firacode nerd font

--- stylix.nix
+++ stylix.nix
@@ -1,10 +1,21 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
 	
 	stylix = {
 		enable = true;
 		image = ./assets/stargate.png;
 		polarity = "dark";
+		base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+		
+		fonts = {
+			monospace = {
+				package = pkgs.fira-code-nerdfont;
+				name = "FiraCode Nerd Font";
+			};
+			serif = config.stylix.fonts.monospace;
+			sansSerif = config.stylix.fonts.monospace;
+			emoji = config.stylix.fonts.monospace;
+		};
 	};
 
 }
