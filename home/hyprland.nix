fbf69867a6eea6e1c5255f78f1be1b7ce952ac6d
nagymathev <nagymathev@gmail.com>
2024-06-10 09:59:15 +0200

    hyprland config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 {
 
+home.packages = with pkgs; [
+	dunst
+	swww
+	ags
+];
+
 xdg.configFile."hypr/hyprland.conf".force = true;
 xdg.configFile."hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink
  	"${config.home.homeDirectory}/nixos/home/config/hyprland.conf";
