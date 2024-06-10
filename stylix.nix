a67219a50b4dd467e8af8021e346045eed33bde5
nagymathev <nagymathev@gmail.com>
2024-06-10 15:51:42 +0200

    change fonts

--- stylix.nix
+++ stylix.nix
@@ -7,16 +7,11 @@
 		polarity = "dark";
 		base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 		
-		cursor.size = 24;
-		
 		fonts = {
 			monospace = {
 				package = pkgs.fira-code-nerdfont;
 				name = "FiraCode Nerd Font";
 			};
-			serif = config.stylix.fonts.monospace;
-			sansSerif = config.stylix.fonts.monospace;
-			emoji = config.stylix.fonts.monospace;
 		};
 		
 		targets.grub.enable = true;
