a2a0722feb8c576d4e940151a771ba9bf202b288
nagymathev <nagymathev@gmail.com>
2024-06-11 17:52:47 +0200

    enable i3 windowmanager

--- desktop.nix
+++ desktop.nix
@@ -5,6 +5,8 @@
 services.xserver.enable = true;
 services.xserver.dpi = 188;
 
+services.xserver.windowManager.i3.enable = true;
+
 # Enable the KDE Plasma Desktop Environment.
 services.desktopManager.plasma6.enable = true;
 services.displayManager.sddm.enable = true;
