b0c268decaa4c0765219d2377ae39b94079d29d7
nagymathev <nagymathev@gmail.com>
2024-06-11 17:50:55 +0200

    set xserver dpi to monitor rated dpi

--- desktop.nix
+++ desktop.nix
@@ -3,6 +3,7 @@
 
 # Enable the X11 windowing system.
 services.xserver.enable = true;
+services.xserver.dpi = 188;
 
 # Enable the KDE Plasma Desktop Environment.
 services.desktopManager.plasma6.enable = true;
