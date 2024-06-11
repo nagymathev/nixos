ef37bc9be1f04c755b8e51af2c34b8ae06cc410b
nagymathev <nagymathev@gmail.com>
2024-06-11 18:40:35 +0200

    enable plasma again

--- desktop.nix
+++ desktop.nix
@@ -8,7 +8,7 @@ services.xserver.dpi = 144;
 services.xserver.windowManager.i3.enable = true;
 
 # Enable the KDE Plasma Desktop Environment.
-services.desktopManager.plasma6.enable = false;
+services.desktopManager.plasma6.enable = true;
 services.displayManager.sddm.enable = true;
 
 programs.hyprland.enable = true;
