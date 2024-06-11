9a5f530bbdba828d67af01e4e01e9aabd90db63b
nagymathev <nagymathev@gmail.com>
2024-06-11 20:50:14 +0200

    change stylix background input

--- stylix.nix
+++ stylix.nix
@@ -3,7 +3,7 @@
 	
 	stylix = {
 		enable = true;
-		image = ./assets/stargate.png;
+		image = ./assets/statue.png;
 		polarity = "dark";
 		base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 		
