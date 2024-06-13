3b6227fe9e7eac507453cf6024d5f08939c49c32
nagymathev <nagymathev@gmail.com>
2024-06-13 18:10:36 +0200

    change cursor to kde breeze, which may fix some non-electron apps incorrectly rendering the cursor size

--- stylix.nix
+++ stylix.nix
@@ -6,6 +6,10 @@
 		image = ./assets/statue.png;
 		polarity = "dark";
 		base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+
+		cursor.package = pkgs.kdePackages.breeze;
+		cursor.name = "Breeze_Light";
+		cursor.size = 24;
 		
 		fonts = {
 			monospace = {
