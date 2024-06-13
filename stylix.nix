b800309642a6332fc8079c5913df544e2ef2c8e1
nagymathev <nagymathev@gmail.com>
2024-06-13 18:31:20 +0200

    change XCURSOR_SIZE global env var to what would represent the 150% scaled 24px default

--- stylix.nix
+++ stylix.nix
@@ -1,25 +1,29 @@
 { config, pkgs, ... }:
 {
 	
-	stylix = {
-		enable = true;
-		image = ./assets/statue.png;
-		polarity = "dark";
-		base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+stylix = {
+	enable = true;
+	image = ./assets/statue.png;
+	polarity = "dark";
+	base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
-		cursor.package = pkgs.kdePackages.breeze;
-		cursor.name = "Breeze_Light";
-		cursor.size = 24;
-		
-		fonts = {
-			monospace = {
-				package = pkgs.fira-code-nerdfont;
-				name = "FiraCode Nerd Font";
-			};
+	cursor.package = pkgs.kdePackages.breeze;
+	cursor.name = "Breeze_Light";
+	cursor.size = 24;
+	
+	fonts = {
+		monospace = {
+			package = pkgs.fira-code-nerdfont;
+			name = "FiraCode Nerd Font";
 		};
-		
-		targets.grub.enable = true;
-		targets.grub.useImage = true;
 	};
+	
+	targets.grub.enable = true;
+	targets.grub.useImage = true;
+};
+
+environment.variables = {
+	XCURSOR_SIZE = 36;
+};
 
 }
