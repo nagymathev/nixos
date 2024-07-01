f6a54ac49946572896bf4c4c36867e89557e05af
nagymathev <nagymathev@gmail.com>
2024-07-01 19:18:24 +0100

    syncthing configure

--- syncthing.nix
+++ syncthing.nix
@@ -3,13 +3,13 @@
 
 services.syncthing = {
 	enable = true;
-	openDefaultPorts = true;
+	# openDefaultPorts = false;
 
 	# Default address for the web gui: 127.0.0.1:8384
 	# To change this use
 	# guiAddress = "127.0.0.1:6969"
 
-	user = "viktor";
+	# user = "viktor";
 };
 
 }
