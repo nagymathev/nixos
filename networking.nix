0b2cf28b8b03765f42a570cfcb36f7b99f405dd5
nagymathev <nagymathev@gmail.com>
2024-08-08 10:09:26 +0100

    Revert "add syncthing hostmap"
    This reverts commit c2c2080ee8b39b790dbfd47a11f6cf4e9bf96f1c.

--- networking.nix
+++ networking.nix
@@ -4,10 +4,6 @@
 networking.hostName = "stellaris"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-networking.hosts = {
-	"127.0.0.1:8384" = [ "syncthing" ];
-};
-
 # Configure network proxy if necessary
 # networking.proxy.default = "http://user:password@proxy:port/";
 # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
