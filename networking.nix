2671aaf002b5ca8e6f9138c605a768dfd050fbe3
nagymathev <nagymathev@gmail.com>
2024-08-09 08:52:19 +0100

    enable bluetooth and wireless support

--- networking.nix
+++ networking.nix
@@ -2,7 +2,7 @@
 {
 
 networking.hostName = "stellaris"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 # Configure network proxy if necessary
 # networking.proxy.default = "http://user:password@proxy:port/";
@@ -19,5 +19,9 @@ networking.networkmanager.enable = true;
 # networking.firewall.allowedUDPPorts = [ ... ];
 # Or disable the firewall altogether.
 # networking.firewall.enable = false;
+
+hardware.bluetooth = {
+	enable = true;
+};
 
 }
