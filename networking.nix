0576e968a72bf6c2bfba6cf29dff8b2544931328
nagymathev <nagymathev@gmail.com>
2024-08-09 08:53:02 +0100

    don't need wireless

--- networking.nix
+++ networking.nix
@@ -2,7 +2,7 @@
 {
 
 networking.hostName = "stellaris"; # Define your hostname.
-networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 # Configure network proxy if necessary
 # networking.proxy.default = "http://user:password@proxy:port/";
