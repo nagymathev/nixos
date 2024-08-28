0ca9441aa3b727bbf06083a3460ca629e78b0a28
nagymathev <nagymathev@gmail.com>
2024-08-28 11:36:28 +0100

    comment tuxedo-drivers

--- systems/stellaris/tuxedo.nix
+++ systems/stellaris/tuxedo.nix
@@ -1,27 +1,24 @@
 { config, pkgs, inputs, ... }:
 {
 
-# Do not enable tuxedo-rs, it makes things run poorly.
-# Well it runs poorly if the profile is set anything but default.
-# This bug is very silly and I hate it.
 # hardware.tuxedo-keyboard.enable = true;
-hardware.tuxedo-rs.enable = true;
-hardware.tuxedo-rs.tailor-gui.enable = true;
 
 disabledModules = [
 	"hardware/tuxedo-keyboard.nix"
 	"services/hardware/tuxedo-rs.nix"
 ];
 
-import = [
+imports = [
 	"${inputs.tuxedo}/nixos/modules/hardware/tuxedo-drivers.nix"
 	"${inputs.tuxedo}/nixos/modules/services/hardware/tuxedo-rs.nix"
 ];
 
 # hardware.tuxedo-drivers.enable = true;
+hardware.tuxedo-rs.enable = true;
+hardware.tuxedo-rs.tailor-gui.enable = true;
 
-boot.kernelPackages = pkgs.linuxKernel.packages.linux_X_X.extend (final: prev: {
-	tuxedo-drivers = inputs.tuxedo.legacyPackages.x86_64-linux.linuxKernel.packages.linux_X_X.tuxedo-drivers;
+boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen.extend (final: prev: {
+	tuxedo-drivers = inputs.tuxedo.legacyPackages.x86_64-linux.linuxKernel.packages.linux_zen.tuxedo-drivers;
 });
 
 # Disable backlight with 0 brightness
