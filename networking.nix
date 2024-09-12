c006410fd6a732bcbf72fec47773a97b8f222e20
nagymathev <nagymathev@gmail.com>
2024-09-12 17:15:50 +0200

    open minecraft ports

--- networking.nix
+++ networking.nix
@@ -15,8 +15,8 @@ networking.networkmanager.enable = true;
 # services.openssh.enable = true;
 
 # Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
+networking.firewall.allowedTCPPorts = [ 25565 ];
+networking.firewall.allowedUDPPorts = [ 25565 ];
 # Or disable the firewall altogether.
 # networking.firewall.enable = false;
 
