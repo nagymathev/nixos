b2b396d8f10486bdc6f49ebbb53d032a93f7bc5d
nagymathev <nagymathev@gmail.com>
2024-09-12 20:58:10 +0200

    default hashed pass for viktor

--- systems/common/users.nix
+++ systems/common/users.nix
@@ -1,16 +1,17 @@
 { config, pkgs, ... }:
 {
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-users.users.viktor = {
-	isNormalUser = true;
-	description = "viktor";
-	extraGroups = [ "networkmanager" "wheel" "docker" ];
-	packages = with pkgs; [
-		firefox
-		discord
-		thunderbird
-	];
-};
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.viktor = {
+    isNormalUser = true;
+    description = "viktor";
+    hashedPassword = "$y$j9T$LGJSMQmQVLLsGxnwwG9Bi/$g7SURzJ453pqVUgJmCotMZ.gBif8cSPK3o16UcxB6.7";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      firefox
+      discord
+      thunderbird
+    ];
+  };
 
 }
