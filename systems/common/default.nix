6ecc591aefdda745113ae3309485a79f7c883c67
nagymathev <nagymathev@gmail.com>
2024-09-12 20:28:57 +0200

    common imports

---
+++ systems/common/default.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+  imports = [
+    ./configuration.nix
+    ./docker.nix
+    ./gc.nix
+    ./keyboard.nix
+    ./localisation.nix
+    ./nix-settings.nix
+    ./system-pkgs.nix
+    ./tmux.nix
+    ./users.nix
+  ];
+}
--- systems/stellaris/default.nix
+++ systems/stellaris/default.nix
@@ -14,15 +14,7 @@
     ./configuration.nix
 
     # Common
-    ../common/configuration.nix
-    ../common/gc.nix
-    ../common/tmux.nix
-    ../common/users.nix
-    ../common/keyboard.nix
-    ../common/system-pkgs.nix
-    ../common/localisation.nix
-    ../common/nix-settings.nix
-    ../common/docker.nix
+    ../common
 
     # Other
     ../../boot.nix
