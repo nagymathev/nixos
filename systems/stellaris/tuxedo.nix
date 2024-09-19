18e2c79746ecaf4996027a8a37e6a44a4ab1513e
nagymathev <nagymathev@gmail.com>
2024-09-19 15:39:02 +0200

    latest kernel

--- systems/stellaris/tuxedo.nix
+++ systems/stellaris/tuxedo.nix
@@ -20,7 +20,7 @@
   hardware.tuxedo-drivers.enable = true;
 
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_X_X.extend (final: prev: {
+    kernelPackages = pkgs.linuxKernel.packages.linux_latest.extend (final: prev: {
       tuxedo-drivers = prev.callPackage (inputs.tuxedo + "/pkgs/os-specific/linux/tuxedo-keyboard/default.nix") {};
     });
 
