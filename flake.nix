84f56f393188526f5d738fe87a85ddd2678362da
nagymathev <nagymathev@gmail.com>
2024-09-19 17:39:51 +0200

    fix typo for equuleus

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
         };
 
         modules = [
-          ./systems/equulius
+          ./systems/equuleus
         ];
       };
     };
--- systems/equuleus/networking.nix
+++ systems/equuleus/networking.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
 
-  networking.hostName = "equulius"; # Define your hostname.
+  networking.hostName = "equuleus"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
