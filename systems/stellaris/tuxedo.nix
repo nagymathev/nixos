0373ed4f96ccfe0d70e5a80fc7d7319a7246dd5e
nagymathev <nagymathev@gmail.com>
2024-09-19 15:13:55 +0200

    kernel override with new drivers

--- systems/stellaris/tuxedo.nix
+++ systems/stellaris/tuxedo.nix
@@ -1,31 +1,31 @@
 { config, pkgs, inputs, ... }:
 {
 
-# hardware.tuxedo-keyboard.enable = true;
+  # hardware.tuxedo-keyboard.enable = true;
 
-disabledModules = [
-	"hardware/tuxedo-keyboard.nix"
-	"services/hardware/tuxedo-rs.nix"
-];
+  disabledModules = [
+    "hardware/tuxedo-keyboard.nix"
+    "services/hardware/tuxedo-rs.nix"
+  ];
 
-imports = [
-	"${inputs.tuxedo}/nixos/modules/hardware/tuxedo-drivers.nix"
-	"${inputs.tuxedo}/nixos/modules/services/hardware/tuxedo-rs.nix"
-];
+  imports = [
+    "${inputs.tuxedo}/nixos/modules/hardware/tuxedo-drivers.nix"
+    "${inputs.tuxedo}/nixos/modules/services/hardware/tuxedo-rs.nix"
+  ];
 
-# hardware.tuxedo-drivers.enable = true;
-hardware.tuxedo-rs.enable = true;
-hardware.tuxedo-rs.tailor-gui.enable = true;
+  # hardware.tuxedo-drivers.enable = true;
+  hardware.tuxedo-rs.enable = true;
+  hardware.tuxedo-rs.tailor-gui.enable = true;
 
-boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen.extend (final: prev: {
-	tuxedo-drivers = inputs.tuxedo.legacyPackages.x86_64-linux.linuxKernel.packages.linux_zen.tuxedo-drivers;
-});
+  kernelPackages = pkgs.linuxPackages.extend (final: prev: {
+    tuxedo-drivers = prev.callPackage (inputs.tuxedo + "/pkgs/os-specific/linux/tuxedo-keyboard/default.nix") {};
+  });
 
-# Disable backlight with 0 brightness
-boot.kernelParams = [
-	"tuxedo_keyboard.mode=0"
-	"tuxedo_keyboard.brightness=0"
-	"tuxedo_keyboard.color_left=0x000000"
-];
+  # Disable backlight with 0 brightness
+  boot.kernelParams = [
+    "tuxedo_keyboard.mode=0"
+    "tuxedo_keyboard.brightness=0"
+    "tuxedo_keyboard.color_left=0x000000"
+  ];
 
 }
