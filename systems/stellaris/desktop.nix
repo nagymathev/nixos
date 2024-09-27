488ea43764d109d4430eebd83a01af4fa4c07308
nagymathev <nagymathev@gmail.com>
2024-09-27 22:24:59 +0200

    add xfce

--- systems/stellaris/desktop.nix
+++ systems/stellaris/desktop.nix
@@ -1,22 +1,24 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
 
-# Enable the X11 windowing system.
-services.xserver.enable = true;
-services.xserver.dpi = 144;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.dpi = 144;
 
-# Enable the KDE Plasma Desktop Environment.
-services.desktopManager.plasma6.enable = false;
-services.displayManager.sddm.enable = false;
+  # Enable the KDE Plasma Desktop Environment.
+  services.desktopManager.plasma6.enable = false;
+  services.displayManager.sddm.enable = false;
 
-services.xserver.desktopManager.gnome.enable = true;
-services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
-programs.hyprland.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 
-environment.systemPackages = with pkgs; [
-	wofi
-	swww
-];
+  programs.hyprland.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wofi
+    swww
+  ];
 
 }
