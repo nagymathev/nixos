c6889c4bc5452466b51997a015a737425ed9f26b
nagymathev <nagymathev@gmail.com>
2024-09-27 15:48:40 +0200

    use custom python module

--- systems/stellaris/configuration.nix
+++ systems/stellaris/configuration.nix
@@ -3,57 +3,61 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, inputs, ... }:
-let
+{
 
-in {
+  imports = [
+    ../common/dev/python.nix
+  ];
 
-environment.variables = {
-	NVIM_PROFILE = "HOME"; # this is for the independent nvim configuration at nagymathev/nvim
-};
+  modules.python.enable = true;
 
-hardware.enableAllFirmware = true;
+  environment.variables = {
+    NVIM_PROFILE = "HOME"; # this is for the independent nvim configuration at nagymathev/nvim
+  };
 
-programs.nix-ld = {
-	enable = true;
-	libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
-};
+  hardware.enableAllFirmware = true;
 
-# Enable CUPS to print documents.
-services.printing.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
+  };
 
-programs.direnv.enable = true;
-programs.gamemode.enable = true;
-programs.gamescope.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-environment.variables.EDITOR = "nvim";
-programs.zsh.enable = true;
-users.defaultUserShell = pkgs.zsh;
-fonts.packages = with pkgs; [
-	fira-code-nerdfont
-];
+  programs.direnv.enable = true;
+  programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
 
-fonts.fontconfig = {
-	enable = true;
-	subpixel.rgba = "rgb";
-	subpixel.lcdfilter = "legacy";
-	hinting.style = "full";
-};
+  environment.variables.EDITOR = "nvim";
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  fonts.packages = with pkgs; [
+    fira-code-nerdfont
+  ];
+
+  fonts.fontconfig = {
+    enable = true;
+    subpixel.rgba = "rgb";
+    subpixel.lcdfilter = "legacy";
+    hinting.style = "full";
+  };
 
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-system.stateVersion = "unstable"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "unstable"; # Did you read the comment?
 
 }
