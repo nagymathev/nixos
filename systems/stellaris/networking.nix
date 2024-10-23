4b65e7e32652104955f347a414708f379101aa4a
nagymathev <nagymathev@gmail.com>
2024-10-23 21:52:08 +0200

    extrahosts for stellaris on the local network

--- systems/stellaris/networking.nix
+++ systems/stellaris/networking.nix
@@ -1,32 +1,37 @@
 { config, pkgs, ... }:
 {
 
-networking.hostName = "stellaris"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "stellaris"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.extraHosts =
+    ''
+    192.168.0.34 equuleus
+    '';
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-# Enable networking
-networking.networkmanager.enable = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-# Open ports in the firewall.
-networking.firewall.allowedTCPPorts = [ 25565 ];
-networking.firewall.allowedUDPPorts = [ 25565 ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-hardware.bluetooth = {
-	enable = true;
-	settings = {
-		General = {
-			Experimental = true;
-		};
-	};
-};
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedUDPPorts = [ 25565 ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
 
 }
