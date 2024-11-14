3416d81b5079ee219770f37e4dd41d3abb570001
nagymathev <nagymathev@gmail.com>
2024-11-14 15:19:33 +0100

    nodejs

--- systems/common/system-pkgs.nix
+++ systems/common/system-pkgs.nix
@@ -1,37 +1,38 @@
 { pkgs, ... }:
 {
 
-environment.systemPackages = with pkgs; [
-	vim
-	neovim
-	git
-	wget
-	curl
-	ripgrep
-	fzf
-	btop
-	just
-	tree
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    git
+    wget
+    curl
+    ripgrep
+    fzf
+    btop
+    just
+    tree
 
-	kitty
+    kitty
 
-	vscode
-	libreoffice
-	vlc
+    vscode
+    libreoffice
+    vlc
 
-	zip
-	xz
-	unzip
-	p7zip
+    zip
+    xz
+    unzip
+    p7zip
 
-	pciutils
-	usbutils
-	lm_sensors
+    pciutils
+    usbutils
+    lm_sensors
 
-	libclang
-	cargo
-	rustc
-	gcc
-];
+    libclang
+    cargo
+    rustc
+    gcc
+    nodejs
+  ];
 
 }
