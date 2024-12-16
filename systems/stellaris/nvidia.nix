eb959a4476ff25b7fb2d51581d926102184cc82d
nagymathev <nagymathev@gmail.com>
2024-12-16 20:12:54 +0100

    remove reverseSync, I have no clue what it does

--- systems/stellaris/nvidia.nix
+++ systems/stellaris/nvidia.nix
@@ -48,7 +48,6 @@ in {
     prime = {
       offload.enable = true;
       offload.enableOffloadCmd = true;
-      reverseSync.enable = true;
 
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:6:0:0";
