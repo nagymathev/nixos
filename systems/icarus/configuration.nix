b92d88f55216d2de1f9c9f50211b6276ce23f034
nagymathev <nagymathev@gmail.com>
2025-02-25 20:53:30 +0100

    icarus: minor changes of configurations as it was copied from stellaris

--- systems/icarus/configuration.nix
+++ systems/icarus/configuration.nix
@@ -19,22 +19,12 @@
     QT_QPA_PLATFORM = ''"wayland;xcb"'';
     QT_STYLE_OVERRIDE = "adwaita";
     EDITOR = "nvim";
-
-    # These are meant to fix what looks to be lower frame rates on external monitors
-#    KWIN_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1"; # setting devices used by kwin (kde)
-#    OGL_DEDICATED_HW_STATE_PER_CONTEXT = "ENABLE_ROBUST"; # nvidia's solution, apparently doesn't work, worth a try
   };
 
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
   };
-
-  services.udev.extraRules = lib.mkMerge [
-    ''
-      ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils-full}/bin/chgrp video /sys/class/backlight/amdgpu_bl1/brightness", RUN+="${pkgs.coreutils-full}/bin/chmod g+w /sys/class/backlight/amdgpu_bl1/brightness"
-    ''
-  ];
 
   hardware.enableAllFirmware = true;
 
