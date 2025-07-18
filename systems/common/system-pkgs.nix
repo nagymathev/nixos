149714896b3f262270e9046d07b7af9c70450ea8
nagymathev <nagymathev@gmail.com>
2025-07-18 17:01:49 +0100

    common: lua version 5.4.7

--- systems/common/system-pkgs.nix
+++ systems/common/system-pkgs.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   environment.systemPackages = with pkgs; [
     vim
     neovim
@@ -46,5 +47,6 @@
     kubectx
     treefmt
     nixfmt-rfc-style
+    lua5_4
   ];
 }
